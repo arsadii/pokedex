a97edd6cf80ad548a5e67bc2c71cdd95ad58d4ec
Arsadi <arsadif55117032@gmail.com>
2023-04-01 12:38:00 +0800

    fix : delete unnecessary component in card

--- src/reusables/designSystem/Card/Card.tsx
+++ src/reusables/designSystem/Card/Card.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import {
-  Card as CCard,
-  CardProps as CCardProps,
-  Grid,
-  GridItem,
-} from "@chakra-ui/react";
+import { Card as CCard, CardProps as CCardProps } from "@chakra-ui/react";
 import Styles from "./Card.module.scss";
 
 type CardProps = CCardProps;
 
 const Card: React.FC<CardProps> = ({ children, ...props }) => {
   return (
-    <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-      <GridItem>
-        <CCard {...props} p={"4"} className={Styles["container"]}>
-          {children}
-        </CCard>
-      </GridItem>
-    </Grid>
+    <CCard p={"4"} className={Styles["container"]} {...props}>
+      {children}
+    </CCard>
   );
 };
 
