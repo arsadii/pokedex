a97951db29ec9b49d6ae0a7539e152cb56f27e59
Arsadi <arsadif55117032@gmail.com>
2023-04-01 10:43:04 +0800

    add : hero section

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Styles from "./Landing.module.scss";
 import Heading from "src/reusables/designSystem/Heading/Heading";
 import Text from "src/reusables/designSystem/Text/Text";
+import PokedexList from "./components/PokedexList";
 
 const Landing: React.FC = () => {
   return (
-    <main className={Styles["app"]}>
-      <Heading>Ini Heading</Heading>
-      <Text>Ini text</Text>
+    <main className={Styles["landing"]}>
+      <div className={Styles["hero"]}>
+        <Heading size={"4xl"}>Pokedex</Heading>
+        <Text fontSize={"2xl"}>Catch your Pokemon</Text>
+      </div>
+      <PokedexList />
     </main>
   );
 };
