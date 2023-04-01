6a77ac44e3230414f3f69c7eeaf32f83ad79a7bd
Arsadi <arsadif55117032@gmail.com>
2023-04-01 10:43:17 +0800

    add : pokedex list component

---
+++ src/pages/Landing/components/PokedexList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Card from "src/reusables/designSystem/Card/Card";
+
+const PokedexList: React.FC = () => {
+  return <Card>Test Card</Card>;
+};
+
+export default PokedexList;
