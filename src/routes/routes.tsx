c638b83d838eb3d0a3074ffd0679f4d073957452
Arsadi <arsadif55117032@gmail.com>
2023-04-04 10:25:43 +0800

    fix : routing

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Landing from "src/pages/Landing/Landing";
+import PokemonDetail from "src/pages/PokemonDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Landing />,
+    children: [
+      {
+        path: "",
+        element: <Landing />,
+      },
+      {
+        path: "detail/:key",
+        element: <PokemonDetail />,
+      },
+    ],
   },
 ]);
 
